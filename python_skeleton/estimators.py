1d14fa96ad0feddc0123ec1c758f63ebe7b49482
Mikhail <mkornilov07@gmail.com>
2025-01-09 18:37:54 -0500

    initial commit

---
+++ python_skeleton/estimators.py
@@ -0,0 +1,11 @@
+class EquityEstimator:
+    def __init__():
+        pass
+    def estimate(gameState, roundState):
+        return
+    
+class MonteCarloEstimator(EquityEstimator):
+    def __init__():
+        super.__init__()
+    def estimate(gameState, roundState):
+        pass
