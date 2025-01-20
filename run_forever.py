0aada56f818738327a429989f4bca7b649e45c6a
jocelyn <jocelynz4890@gmail.com>
2025-01-20 03:12:18 -0500

    train on pc

--- run_forever.py
+++ run_forever.py
@@ -5,11 +5,13 @@ def main():
     n = 0
     while True:
         n = n + 1
+        # user_input = input()
+        # if user_input.lower() == 'n':
         print(f'Starting round {n}')
 
         process = subprocess.run(["python3", "engine.py"], capture_output=True, text=True)
         
-        time.sleep(1)
+        # time.sleep(1)
 
 if __name__ == "__main__":
     main()
